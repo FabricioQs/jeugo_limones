9d7fd3e93833fb2aac702816a70e04a1761dfd49
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-06-25 22:28:52 -0500

    Parte 2: Modificación de velocidadCaida según puntaje y mensaje creativo de GANADOR

--- limones.js
+++ limones.js
@@ -14,7 +14,7 @@ let limonX=canvas.width/2;
 let limonY=0;
 let puntaje=0;
 let vidas=3;
-let velocidadCaida=100;
+let velocidadCaida=200;
 
 function iniciar(){
     setInterval(bajarLimon,velocidadCaida);
@@ -77,6 +77,14 @@ function detectarAtrapado (){
        aparecerLimon();
        puntaje=puntaje+1
        mostrarEnSpan("txtPuntaje",puntaje);
+
+       if (puntaje===3){
+        velocidadCaida=150;
+       } else if (puntaje===6){
+         velocidadCaida=100;
+       } else if (puntaje===10){
+        alert ("Felicidades Tienes los limones, ahora te falta la sal y el tequila")
+       }
     }
 }
 
